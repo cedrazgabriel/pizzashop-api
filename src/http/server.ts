1ebec7434484503ec57f59a92e387fc091bd5072
cedrazgabriel <cedrazdev@gmail.com>
2024-02-14 19:31:01 -0300

    chore: initial commit

---
+++ src/http/server.ts
@@ -0,0 +1,11 @@
+import { Elysia } from 'elysia'
+
+const app = new Elysia()
+    .get('/', () => {
+        return 'hello world'
+    }
+    )
+
+app.listen(3333, () => {
+    console.log('Server is running on port 3333')
+})
