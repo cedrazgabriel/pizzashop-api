a93029e0a3ebe0c2a62b08cdb617b64c10082ea6
Gabriel Cedraz <cedrazdev@gmail.com>
2024-02-25 19:59:01 -0300

    feat: create seed file on database

---
+++ src/db/seed.ts
@@ -0,0 +1,55 @@
+import { faker} from '@faker-js/faker'
+import { users, restaurants } from './schema'
+import { db } from './connection'
+import chalk from 'chalk'
+
+//Reset database
+
+await db.delete(users)
+await db.delete(restaurants)
+
+console.log(chalk.yellow('Database reset!'))
+
+//Create costumers
+await db.insert(users).values([
+    {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer'
+    },
+    {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    role: 'customer'
+    },
+])
+
+console.log(chalk.yellow('Costumers created!'))
+
+//Create manager
+const [manager] =   await db.insert(users).values([
+    {
+    name: faker.person.fullName(),
+    email: 'admin@admin.com',
+    role: 'manager'
+    }
+]).returning({
+    id: users.id
+})
+
+console.log(chalk.yellow('Manager created!'))
+
+//Create restaurant 
+await db.insert(restaurants).values([
+    {
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    managerId: manager.id
+    },
+])
+
+console.log(chalk.yellow('Restaurant created!'))
+
+console.log(chalk.green('Seed completed!'))
+
+process.exit(0)
