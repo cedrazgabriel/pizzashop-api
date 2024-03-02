c6118200cb86587d3cb707f1b043588211db9aa2
Gabriel Cedraz <cedrazdev@gmail.com>
2024-03-02 10:32:20 -0300

    feat: create custom unauthorized errror and implement handling error on server.ts and auth.ts

--- src/http/auth.ts
+++ src/http/auth.ts
@@ -2,12 +2,23 @@ import cookie from '@elysiajs/cookie'
 import jwt from '@elysiajs/jwt'
 import { Elysia, t, type Static } from 'elysia'
 import { env } from '../env'
+import { UnauthorizedError } from './errors/unauthorized-error'
 
 const jwtPayload = t.Object({
   sub: t.String(), // Usuário a quem se refere esse token
   restaurantId: t.Optional(t.String()),
 })
 export const auth = new Elysia()
+  .error({
+    UNAUTHORIZED: UnauthorizedError,
+  })
+  .onError(({ error, code, set }) => {
+    switch (code) {
+      case 'UNAUTHORIZED':
+        set.status = 401
+        return { code, message: error.message }
+    }
+  })
   .use(
     jwt({
       secret: env.JWT_SECRET_KEY,
@@ -40,7 +51,7 @@ export const auth = new Elysia()
 
         // Se não conseguiu verificar a assinatura
         if (!payload) {
-          throw new Error('Unauthorized')
+          throw new UnauthorizedError()
         }
 
         // Devolve os dados do token descriptografados
