1f0d74055d8902243f0cff3753c41d14ebd5eeb5
Gabriel Cedraz <cedrazdev@gmail.com>
2024-03-14 19:42:14 -0300

    feat: update seed file

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,30 +1,44 @@
 /* eslint-disable drizzle/enforce-delete-with-where */
 
 import { faker } from '@faker-js/faker'
-import { users, restaurants } from './schema'
+import {
+  users,
+  restaurants,
+  products,
+  ordersItems,
+  orders,
+  authLinks,
+} from './schema'
 import { db } from './connection'
 import chalk from 'chalk'
+import { createId } from '@paralleldrive/cuid2'
 
 // Reset database
-
 await db.delete(users)
 await db.delete(restaurants)
+await db.delete(ordersItems)
+await db.delete(orders)
+await db.delete(products)
+await db.delete(authLinks)
 
 console.log(chalk.yellow('Database reset!'))
 
 // Create costumers
-await db.insert(users).values([
-  {
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-    role: 'customer',
-  },
-  {
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-    role: 'customer',
-  },
-])
+const [customer1, customer2] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      role: 'customer',
+    },
+    {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      role: 'customer',
+    },
+  ])
+  .returning()
 
 console.log(chalk.yellow('Costumers created!'))
 
@@ -45,15 +59,93 @@ const [manager] = await db
 console.log(chalk.yellow('Manager created!'))
 
 // Create restaurant
-await db.insert(restaurants).values([
-  {
-    name: faker.company.name(),
-    description: faker.lorem.paragraph(),
-    managerId: manager.id,
-  },
-])
+const [restaurant] = await db
+  .insert(restaurants)
+  .values([
+    {
+      name: faker.company.name(),
+      description: faker.lorem.paragraph(),
+      managerId: manager.id,
+    },
+  ])
+  .returning()
 
 console.log(chalk.yellow('Restaurant created!'))
+
+// Create products
+function generateProducts() {
+  return {
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    priceInCents: Number(faker.commerce.price({ min: 190, max: 490, dec: 0 })),
+    restaurantId: restaurant.id,
+  }
+}
+const availableProducts = await db
+  .insert(products)
+  .values([
+    generateProducts(),
+    generateProducts(),
+    generateProducts(),
+    generateProducts(),
+    generateProducts(),
+    generateProducts(),
+  ])
+  .returning()
+
+console.log(chalk.yellow('Products created!'))
+
+// Create orders
+type OrdemItemsInserts = typeof ordersItems.$inferInsert
+type OrderInsert = typeof orders.$inferInsert
+
+const orderItemsToInsert: OrdemItemsInserts[] = []
+const orderToInsert: OrderInsert[] = []
+
+for (let i = 0; i < 200; i++) {
+  const orderId = createId()
+
+  const orderProducts = faker.helpers.arrayElements(availableProducts, {
+    min: 1,
+    max: 3,
+  })
+
+  let totalInCents = 0
+
+  orderProducts.forEach((orderProduct) => {
+    const quantity = faker.number.int({ min: 1, max: 3 })
+
+    totalInCents += orderProduct.priceInCents * quantity
+
+    orderItemsToInsert.push({
+      orderId,
+      priceInCents: orderProduct.priceInCents,
+      quantity,
+      productId: orderProduct.id,
+    })
+  })
+
+  orderToInsert.push({
+    id: orderId,
+    customerId: faker.helpers.arrayElement([customer1.id, customer2.id]),
+    restaurantId: restaurant.id,
+    totalInCents,
+    status: faker.helpers.arrayElement([
+      'pending',
+      'processing',
+      'delivering',
+      'delivered',
+      'canceled',
+    ]),
+    createdAt: faker.date.recent({ days: 40 }),
+  })
+}
+
+await db.insert(orders).values(orderToInsert)
+console.log(chalk.yellow('Orders created!'))
+
+await db.insert(ordersItems).values(orderItemsToInsert)
+console.log(chalk.yellow('Order items created!'))
 
 console.log(chalk.green('Seed completed!'))
 
