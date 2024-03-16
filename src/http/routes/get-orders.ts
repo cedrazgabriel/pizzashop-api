989747203a232ddc18871df922a32b4341dc3fee
Gabriel Cedraz <cedrazdev@gmail.com>
2024-03-16 11:31:43 -0300

    feat: create get orders route

---
+++ src/http/routes/get-orders.ts
@@ -0,0 +1,65 @@
+import { Elysia, t } from 'elysia'
+import { auth } from '../auth'
+import { db } from '../../db/connection'
+import { UnauthorizedError } from '../errors/unauthorized-error'
+import { createSelectSchema } from 'drizzle-typebox'
+import { orders, users } from '../../db/schema'
+import { and, count, eq, getTableColumns, ilike } from 'drizzle-orm'
+
+export const getOrders = new Elysia().use(auth).get(
+  '/orders',
+  async ({ getCurrentUser, query }) => {
+    const { restaurantId } = await getCurrentUser()
+    const { customerName, orderId, status, pageIndex } = query
+
+    if (!restaurantId) {
+      throw new UnauthorizedError()
+    }
+
+    // Função nativa do Drizzle que pega todas as colunas de uma tabela
+    const orderTableCollums = getTableColumns(orders)
+
+    // Faz uma base query com todos os filtros opcionais
+    const baseQuery = db
+      .select(orderTableCollums)
+      .from(orders)
+      .innerJoin(users, eq(users.id, orders.customerId))
+      .where(
+        and(
+          eq(orders.restaurantId, restaurantId),
+          orderId ? ilike(orders.id, `%${orderId}%`) : undefined,
+          status ? eq(orders.status, status) : undefined,
+          customerName ? ilike(users.name, `%${customerName}%`) : undefined,
+        ),
+      )
+
+    // Aqui, ele faz duas consultas ao banco, uma para pegar a quantidade de pedidos e outra para pegar os pedidos em si
+    const [amountOfOrdersQuery, allOrders] = await Promise.all([
+      db.select({ count: count() }).from(baseQuery.as('baseQuery')),
+      db
+        .select()
+        .from(baseQuery.as('baseQuery'))
+        .offset(pageIndex * 10)
+        .limit(10),
+    ])
+
+    const countOfOrders = amountOfOrdersQuery[0].count
+
+    return {
+      orders: allOrders,
+      meta: {
+        pageIndex,
+        perPage: 10,
+        total: countOfOrders,
+      },
+    }
+  },
+  {
+    query: t.Object({
+      customerName: t.Optional(t.String()),
+      orderId: t.Optional(t.String()),
+      status: t.Optional(createSelectSchema(orders).properties.status),
+      pageIndex: t.Numeric({ minimum: 0 }),
+    }),
+  },
+)
